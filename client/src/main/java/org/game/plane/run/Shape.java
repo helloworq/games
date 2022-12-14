a115d17eda658eba690781164dfa65fa9e36b2f0
lei.zhou <1273370082@qq.com>
2022-12-14 13:35:20 +0800

    plane: 支持绘制指定形状

--- client/src/main/java/org/game/plane/run/Shape.java
+++ client/src/main/java/org/game/plane/run/Shape.java
@@ -2,17 +2,19 @@ package org.game.plane.run;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.game.plane.event.KeyMointer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-/**
- * 测试各种运动曲线函数的生成
- */
-public class Run {
+public class Shape {
+
+    private static int x = 200;
+    private static int y = 200;
+    private static int rotate = 0;
 
     @Getter
     @AllArgsConstructor
@@ -26,6 +28,32 @@ public class Run {
             this.setBounds(0, 0, 400, 400); //设置窗体大小和位置
             this.setVisible(true); //使用该属性才能显示窗体
             //实现程序运行关闭的功能
+            addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    switch (e.getKeyChar()) {
+                        case 'w':
+                            y -= 20;
+                            break;
+                        case 'a':
+                            x -= 20;
+                            break;
+                        case 's':
+                            y += 20;
+                            break;
+                        case 'd':
+                            x += 20;
+                            break;
+                        case 'j':
+                            rotate -= 20;
+                            break;
+                        case 'k':
+                            rotate += 20;
+                            break;
+                    }
+
+                }
+            });
             this.addWindowListener(new WindowAdapter() {
                 public void windowClosing(WindowEvent e) {
                     System.exit(-1);
@@ -50,28 +78,29 @@ public class Run {
         double angle = 0;
 
         private void drawRect(Graphics g) {
-            Run.Position position = getCirclePosition(0);
+            Run.Position position = getCirclePosition(0.0);
             Run.Position position2 = getCirclePosition(1.0 / 3.0);
             Run.Position position3 = getCirclePosition(2.0 / 3.0);
-            Run.Position position4 = getCirclePosition(3.0 / 3.0);
+            Run.Position position4 = getCirclePosition(1.0);
             Run.Position position5 = getCirclePosition(4.0 / 3.0);
             Run.Position position6 = getCirclePosition(5.0 / 3.0);
-            Run.Position position7 = getCirclePosition(6.0 / 3.0);
 
-            g.drawRect(200, 200, 20, 20);
+            //g.drawRect(200, 200, 20, 20);
+            //g.drawOval(200, 200, 20, 20);
+            //g.drawArc(200, 200, 20, 20, 225, 315);
+            g.fillArc(x, y, 20, 20, rotate - 30, -60);
 
-            g.drawRect(200 + position.x, 200 + position.y, 20, 20);
-            g.drawRect(200 + position2.x, 200 + position2.y, 20, 20);
-            g.drawRect(200 + position3.x, 200 + position3.y, 20, 20);
-            g.drawRect(200 + position4.x, 200 + position4.y, 20, 20);
-            g.drawRect(200 + position5.x, 200 + position5.y, 20, 20);
-            g.drawRect(200 + position6.x, 200 + position6.y, 20, 20);
-            g.drawRect(200 + position7.x, 200 + position7.y, 20, 20);
+            g.drawOval(x + position.x, y + position.y, 20, 20);
+            g.drawRect(x + position2.x, y + position2.y, 20, 20);
+            g.fillArc(x + position3.x, y + position3.y, 20, 20, rotate - 30, -60);
+            g.drawRect(x + position4.x, y + position4.y, 20, 20);
+            g.drawOval(x + position5.x, y + position5.y, 20, 20);
+            g.drawRect(x + position6.x, y + position6.y, 20, 20);
 
-            angle = angle < 2 ? (angle + 1.0 / 128.0) : 0.0;
+            angle = angle < 2 ? (angle + 1.0 / 256.0) : 0.0;
         }
 
-        public Position getCirclePosition(double offset) {
+        public Run.Position getCirclePosition(double offset) {
             //根据直径计算坐标(加上距圆心偏移量即为坐标)
             double curAngle = offset + angle;
 
@@ -79,9 +108,8 @@ public class Run {
             int y = (int) (Math.sin(curAngle * Math.PI) * length);
             int x = (int) (Math.cos(curAngle * Math.PI) * length);
 
-
             //System.out.println("角度: " + angle + " X坐标: " + x + " Y坐标: " + y + " 计算和" + Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
-            return new Position(x, y);
+            return new Run.Position(x, y);
         }
     }
 
